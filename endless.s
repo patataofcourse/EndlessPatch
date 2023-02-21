eb528aebaaf28f528c679d2a813098a364dd87d5
patataofcourse <patataofcourse@gmail.com>
2023-02-21 17:34:34 +0100

    some optimizations

--- endless.s
+++ endless.s
@@ -1,12 +1,13 @@
 ; EndlessPatch rev3 - unified version
 ;   shouldn't break vanilla games anymore, so it should be safe enough to add
+;   incompatible with RHMPatch >= v1.3 for now
 
 .arm.little
 .open "original.bin", "code.bin", 0x100000
 
 ; our own offsets
 ENDLESS_tickPatch   equ 0x0025c3c0
-ENDLESS_newCode     equ 0x00399b00 ; earliest it can theoretically be done is 00399adc
+ENDLESS_newCode     equ 0x00399f00
 
 ; vanilla game functions called
 cmdreturn           equ 0x002613cc
@@ -40,16 +41,15 @@ ENDLESS_cmd_BF:
 ENDLESS_saveScore:
     stmdb sp!, {r4, r5, r6, lr}
     mov r6, r0
-    ldr r5, [_0054d350]
+    ldr r5, =D_0054d350
 
     ; check current game
-    ldr r0, [_0054ef10]
+    ldr r0, =D_0054ef10
     ldr r0, [r0]
     ldrb r4, [r0, 0x4c] ; current gate slot?
     
-    ; must fit format 0000XX11
-    mvn r1, 0b1100
-    and r1, r1, 0xFF
+    ; must only be run on the endless game
+    mov r1, 0b11
     and r1, r4, r1
     cmp r1, #3
     bne _return
@@ -57,8 +57,8 @@ ENDLESS_saveScore:
     ; get saved score
     ldr r0, [r5]
     mov r1, r4
-    mvn r2, #0      ; r2 = 0xFFFFFFFF
-    bl getGateScore
+    mvn r2, #0      ; r2 = -1
+    bl getEndlessScore
 
     ; if the saved score is bigger or equal to the current score, don't save
     cmp r0, r6
@@ -68,20 +68,17 @@ ENDLESS_saveScore:
     ldr r0, [r5]
     mov r1, r4
     mov r2, r6
-    mvn r3, #0      ; r3 = 0xFFFFFFFF
-    bl setGateScore
+    mvn r3, #0      ; r3 = -1
+    bl setEndlessScore
 
     ; save the game
-    ldr r0, [_0054ef28]
+    ldr r0, =D_0054ef28
     ldr r0, [r0]
-    ldmia sp!, {r4, r5, r6, lr}
-    b saveGame
+    bl saveGame
 
 _return:
     ldmia sp!, {r4, r5, r6, pc}
-_0054d350: .word D_0054d350
-_0054ef10: .word D_0054ef10
-_0054ef28: .word D_0054ef28
 
+.pool
 
 .close
