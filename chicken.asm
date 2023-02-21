90349b71d8e6aac2627f190ddcd1b2554c26cc12
patataofcourse <patataofcourse@gmail.com>
2023-02-21 17:03:45 +0100

    oh my god lmao

--- chicken.asm
+++ chicken.asm
@@ -1,37 +1,8 @@
 .arm.little
 .open "original.bin", "code.bin", 0x100000
 
-chickenCmd equ 0x165b04
-chickenSaveScore equ 0x1ecc58
-chickenGEq equ 0x1ecc98
-chickenCondvar equ 0x399fd0
-
-saveGame equ 0x28bf14
-getEndlessScore equ 0x261a6c
-chickenContinue equ 0x1ecc78
-
-.org chickenCmd
-    ldreq r1,[r4,#0x20]
-    bleq chickenSaveScore
-
-.org chickenSaveScore
-    stmdb sp!,{r4, r5, r6, r9, lr}
-    mov r4,r0
-    mov r9,r1
-    ldr r5,[0x1ecca4]
-    mvn r2,#0x0
-    mov r1,#0xf
-    ldr r0,[r5,#0x0]
-    b chickenCondvar
-
-.org chickenGEq
-    ldmia sp!, {r4, r5, r6, r9, lr}
-    b saveGame
-    ldmia sp!, {r4, r5, r6, r9, pc}
-
-.org chickenCondvar
-    bl getEndlessScore
-    mov r2, r9
-    b chickenContinue
+; load condvar instead of endless score in CSceneCtrChicken::SaveScore function
+.org 0x1ecc74
+    ldr r2, [r4, #0x20]
 
 .close
--- coin.asm
+++ coin.asm
@@ -1,36 +1,8 @@
 .arm.little
 .open "original.bin", "code.bin", 0x100000
 
-coinCmd equ 0x18715c
-coinSaveScore equ 0x295ce4
-coinGEq equ 0x295d30
-coinCondvar equ 0x399ff4
-
-getEndlessScore equ 0x261a6c
-coinContinue equ 0x295d04
-
-.org coinCmd
-    ldr r1,[r4,#0x20]
-    bl coinSaveScore
-
-.org coinSaveScore
-    stmdb sp!,{r4, r5, r6, r9, lr}
-    mov r4,r0
-    mov r9,r1
-    ldr r5,[0x295d3c]
-    mvn r2,#0x0
-    mov r1,#0x7
-    ldr r0,[r5,#0x0]
-    b coinCondvar
-
-.org coinGEq
-    ldmia sp!, {r4, r5, r6, r9, lr}
-    b 0x23663c
-    ldmia sp!, {r4, r5, r6, r9, pc}
-
-.org coinCondvar
-    bl getEndlessScore
-    mov r2, r9
-    b coinContinue
+; load condvar instead of endless score in CSceneNtrCoinToss::SaveScore function
+.org 0x295d00
+    ldr r2, [r4, #0x20]
 
 .close
